9233949aa1deeb011b0afa48c49543f94227a0e1
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-06-02 16:41:45 -0300

    npm install after git pull

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -3,14 +3,12 @@ import LoginRouter from './routes/loginRoute';
 
 class App {
   public app: express.Express;
-  // ...
 
   constructor() {
     this.app = express();
     this.app.use(express.json());
     this.config();
     this.routesSelector();
-    // ...
   }
 
   private config():void {
@@ -25,7 +23,6 @@ class App {
     // ...
   }
 
-  // ...
   public start(PORT: string | number):void {
     this.app.listen(PORT);
   }
