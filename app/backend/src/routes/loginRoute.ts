2e7d3d6d3ee3013b15156f9fb76f2a89681cea93
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-06-03 20:48:30 -0300

    Add 11, 13

--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import LoginController from '../controllers/loginController';
+import LoginMiddleware from '../middlewares/loginMiddleware';
 
 const router = Router();
 
-router.post('/login', LoginController.postLogin);
+router.post('/login', LoginMiddleware.checkInfo, LoginController.postLogin);
 
 export default router;
