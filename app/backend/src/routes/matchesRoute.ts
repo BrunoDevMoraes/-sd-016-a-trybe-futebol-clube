45f784365de8de89d5ffed2308ee7ce7d2bebf07
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-06-04 01:11:26 -0300

    Add 23

--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import CheckAuth from '../middlewares/checkAuth';
 import MatchesController from '../controllers/matchesController';
 
 const router = Router();
 
 router.get('/matches', MatchesController.getAll);
+
+router.post('/matches', CheckAuth.jwtValidator, MatchesController.createMatch);
 
 export default router;
