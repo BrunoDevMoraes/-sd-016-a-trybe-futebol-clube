73324543616193b71fbcfc08f57525ec8abc1038
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-06-03 22:12:20 -0300

    Add 17

--- app/backend/src/routes/teamsRoute.ts
+++ app/backend/src/routes/teamsRoute.ts
@@ -3,6 +3,7 @@ import TeamsController from '../controllers/teamsController';
 
 const router = Router();
 
+router.get('/teams/:id', TeamsController.getById);
 router.get('/teams', TeamsController.getAll);
 
 export default router;
--- app/backend/src/services/teamsService.ts
+++ app/backend/src/services/teamsService.ts
@@ -5,4 +5,9 @@ export default class TeamsService {
     const teams = Teams.findAll();
     return teams;
   }
+
+  static async getById(id: string) {
+    const team = Teams.findByPk(id);
+    return team;
+  }
 }
